082cd09e0ea32e83d7af6ef6739192c60734ec0c
Ishita Ganjoo <iganjoo@silo.soic.indiana.edu>
2016-11-20 00:25:20 -0500

    Cleaning up

--- spamClassification.py
+++ spamClassification.py
@@ -1,10 +1,12 @@
 import os
 
 class spamClassification:
-	countSpam,countNSpam = 0,0
-	priorSpam,priorNSpam= 0,0
+	def __init__(self):
+		self.priorSpam = 0
+        self.priorNSpam = 0
+	
 	def createDictionary(self):
-		
+		countSpam,countNSpam = 0,0
 		dirNSpam = os.listdir("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/notspam")
 		dirSpam = os.listdir("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/spam")
 		notSpamDict = {}
@@ -12,7 +14,7 @@ class spamClassification:
 		mailList= []
 		
 		for fileName in dirNSpam:
-			countNSpam+=1
+			countNSpam += 1
 			with open("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/notspam/"+fileName, "r") as f:
 				content = f.readlines()
 				newStr = "".join(content)
@@ -20,13 +22,13 @@ class spamClassification:
 		for mail in mailList:
 			for eachWord in mail:
 				if (notSpamDict.get(eachWord) != None):
-					notSpamDict[eachWord] = notSpamDict[eachWord] + 1
+					notSpamDict[eachWord] += 1
 				else:
 					notSpamDict[eachWord] = 1
 
 		mailList = []
 		for fileName in dirSpam:
-			countSpam+=1
+			countSpam += 1
 			with open("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/spam/"+fileName, "r") as f:
 				content = f.readlines()
 				newStr = "".join(content)
@@ -34,15 +36,13 @@ class spamClassification:
 		for mail in mailList:
 			for eachWord in mail:
 				if(spamDict.get(eachWord) != None):
-					spamDict[eachWord] = spamDict[eachWord] + 1
+					spamDict[eachWord] += 1
 				else:
 					spamDict[eachWord] = 1
-		priorSpam = countSpam/(countSpam+countNSpam)
-		priorNSpam = countNSpam/(countSpam+countNSpam)			
-		#print(notSpamDict)
-		#print(spamDict)
-		print("prior Spam",priorSpam)
-		print("prior Non Spam",priorNSpam)
-
+		self.priorSpam = countSpam/(countSpam+countNSpam)
+		self.priorNSpam = countNSpam/(countSpam+countNSpam)			
+		print("prior Spam",self.priorSpam)
+		print("prior Non Spam",self.priorNSpam)
+		
 spamObj = spamClassification()
 spamObj.createDictionary()
