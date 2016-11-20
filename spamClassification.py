5005a24c2eb102eef585bac257501d20234e7c00
Ishita Ganjoo <iganjoo@silo.soic.indiana.edu>
2016-11-19 23:19:15 -0500

    commit

--- spamClassification.py
+++ spamClassification.py
@@ -1,45 +1,48 @@
 import os
 
 class spamClassification:
+	countSpam,countNSpam = 0,0
+	priorSpam,priorNSpam= 0,0
 	def createDictionary(self):
-		path = "/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/notspam"
-		path2 = "/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/spam"
-		dirs = os.listdir(path)
-		dirs2 = os.listdir(path2)
+		
+		dirNSpam = os.listdir("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/notspam")
+		dirSpam = os.listdir("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/spam")
 		notSpamDict = {}
 		spamDict = {}
-		wordList= []
-
-		for fileName in dirs:
+		mailList= []
+		
+		for fileName in dirNSpam:
+			countNSpam+=1
 			with open("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/notspam/"+fileName, "r") as f:
 				content = f.readlines()
 				newStr = "".join(content)
-				wordList.append(newStr.split(' '))
-		for word in wordList:
-			for eachWord in word:
+				mailList.append(newStr.split(' '))
+		for mail in mailList:
+			for eachWord in mail:
 				if (notSpamDict.get(eachWord) != None):
-					value = notSpamDict[eachWord]
-					value += 1
-					notSpamDict[eachWord] = value
+					notSpamDict[eachWord] = notSpamDict[eachWord] + 1
 				else:
 					notSpamDict[eachWord] = 1
 
-		wordList = []
-		for fileName in dirs2:
+		mailList = []
+		for fileName in dirSpam:
+			countSpam+=1
 			with open("/u/bansalro/csci_b551_assignment_4/anahar-bansalro-iganjoo-a4/part1/part1/train/spam/"+fileName, "r") as f:
 				content = f.readlines()
 				newStr = "".join(content)
-				wordList.append(newStr.split(' '))
-		for word in wordList:
-			for eachWord in word:
+				mailList.append(newStr.split(' '))
+		for mail in mailList:
+			for eachWord in mail:
 				if(spamDict.get(eachWord) != None):
-					value = spamDict[eachWord]
-					value += 1
-					spamDict[eachWord] = value
+					spamDict[eachWord] = spamDict[eachWord] + 1
 				else:
 					spamDict[eachWord] = 1
-		print(notSpamDict)
-		print(spamDict)
+		priorSpam = countSpam/(countSpam+countNSpam)
+		priorNSpam = countNSpam/(countSpam+countNSpam)			
+		#print(notSpamDict)
+		#print(spamDict)
+		print("prior Spam",priorSpam)
+		print("prior Non Spam",priorNSpam)
 
 spamObj = spamClassification()
 spamObj.createDictionary()
